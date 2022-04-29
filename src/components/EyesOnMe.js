2c5cc958115260f1b352b3b1a618e12e5a3ce969
Brooke Strand <brooke.strand007@gmail.com>
2022-04-29 14:49:19 -0400

    Completed assignment

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,15 @@
 // Code EyesOnMe Component Here
+import React from 'react';
+
+function EyesOnMe() {
+    function logFocus() {
+        console.log("Good!")
+    }
+    function logBlur() {
+        console.log("Hey! Eyes on me!")
+    }
+    return(
+        <button onFocus={logFocus} onBlur={logBlur}>Eyes on me</button>
+    )
+}
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,12 @@
 // Code Keypad Component Here
+import React from 'react';
+
+function KeyPad () {
+    function logPass() {
+        console.log("Entering password...")
+    }
+    return(
+        <input  onChange={logPass} type="password" />
+    )   
+}
+export default KeyPad;
